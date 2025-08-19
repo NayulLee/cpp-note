9538a85a52a4f44a600c95b40af1795dba2a8ad5
Nayul <ahrvhvrmsghk@gmail.com>
2025-08-19 11:02:33 +0900

    feat : 계좌 개설 시 입력값 유효성 검사 코드 추가

--- BankingSystem/BankingSystem.cpp
+++ BankingSystem/BankingSystem.cpp
@@ -1,6 +1,7 @@
 #include "BankingSystem.h"
 #include <iostream>
 #include <cstring>
+#include <cctype>
 
 Account* accArr[100];  // 최대 100개까지 계좌 생성 가능
 int accNum = 0;
@@ -47,7 +48,7 @@ Account::~Account()
 
 void showMenu(void)
 {
-    std::cout << "-----------------Meny-----------------" << std::endl;
+    std::cout << "-----------------Menu-----------------" << std::endl;
     std::cout << "1. 계좌 개설" << std::endl;
     std::cout << "2. 입     금" << std::endl;
     std::cout << "3. 출     금" << std::endl;
@@ -57,17 +58,45 @@ void showMenu(void)
 
 void makeAccount(void)
 {
+    try{    // 유효성 검사
     int id;
     char name[NAME_LEN];
     int balance;
 
     std::cout << "[계좌 개설]" << std::endl;
     std::cout << "계좌 번호 입력 : " ;   std::cin >> id;
-    std::cout << "이름  입력 : " ;   std::cin >> name;
-    std::cout << "입금액 입력 : " ;   std::cin >> balance;
-    std::cout << std::endl;    
+    if(std::cin.fail()) throw std::invalid_argument("계좌 번호는 숫자만 가능합니다.");
 
-    accArr[accNum++] = new Account(id, balance, name);
+    std::cout << "이름  입력 : " ;   std::cin >> name;
+    for(int i = 0; name[i] != '\0'; i++){
+        if(isdigit(name[i]) || ispunct(name[i]) || isspace(name[i])) // 숫자 || 특문 || 스페이스
+            throw std::invalid_argument("이름에는 숫자/특수문자를 넣을 수 없습니다.");
+    }
+
+    std::cout << "입금액 입력 : " ;   std::cin >> balance;
+    if(balance < 0) throw std::invalid_argument("입금액은 0이상이어야 합니다.");
+
+        accArr[accNum++] = new Account(id, balance, name);
+
+    std::cout << "-------------------계좌 개설 완료!-------------------"<< std::endl;    
+    }
+    catch(const std::exception& e){
+        std::cerr << "ERROR : " << e.what() << std:: endl;
+        std::cin.clear();
+        std::cin.ignore(100, '\n');
+        /*
+        > 입력 스트림(cin)의 상태를 초기화하고 남은 입력을 버린다.
+        > !!!입력값 유효성 검사 후 무조건 넣어줘야하는 코드!!!
+         std::cin.clear();
+         - 입력 도중 오류가 발생하면(ex.숫자 입력인데 문자 입력) failbit가 켜지고 입력이 전부 막힌다
+         - 'cin.clear()' : 입력 스트림의 상태 플래그를 초기화(clear)해서 다시 사용 가능하게 만드는 함수
+
+        std::cin.ignore(100, '\n'); 
+         - 입력 버퍼에는 사용자가 입력하고 엔터 친 값이 남아있다(개행 문자)
+         - 'cin.ignore(n, delim)': 버퍼에서 n개 문자 혹은 delim(구분자)까지 버린다.
+         - 여기서는 최대 100문자, 또는 개행 문자까지 버리면서 다음 입력 준비 완료시킨다.
+        */
+    }
 }
 
 void depositMoney(void)
